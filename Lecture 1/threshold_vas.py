13a27144d1eb3338eb559e6e494258a661da5d72
vismiroglou <vismiroglou@gmail.com>
2023-09-09 07:37:50 +0200

    turned thresholding algorithm into function

--- Lecture 1/threshold_vas.py
+++ Lecture 1/threshold_vas.py
@@ -10,21 +10,20 @@ import cv2 as cv
 import numpy as np
 import sys
 
-if __name__ == '__main__':
-    img = cv.imread('images/dots.jpg')
+
+def man_threshold(img, th):
     img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    
-    #Manually set global threshold
-    th = int(input('Select a threshold value between 0 and 255: '))
+
     for counteri, i in enumerate(img):
         for counterj, _ in enumerate(i):
             if img[counteri, counterj] < th:
                 img[counteri, counterj] = 0
             else:
                 img[counteri, counterj] = 255
-    
-    print(img)
-    cv.imshow('Threshold', img)
-    cv.waitKey(0)
+
+    return img
+
 
     
+    
+
