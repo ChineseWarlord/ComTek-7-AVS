fbc819a15771b1230eb9730177978448db812054
vism <vismiroglou@gmail.com>
2023-09-19 10:29:54 +0200

    Started mean background

--- Lecture 4/Vas/background.py
+++ Lecture 4/Vas/background.py
@@ -2,8 +2,9 @@ import cv2 as cv
 import numpy as np
 import glob
 
-bg =cv.imread('../Data/Test016/001.tif')
-bg = cv.cvtColor(bg, cv.COLOR_BGR2GRAY).astype(np.float32)
+# bg =cv.imread('../Data/Test016/001.tif')
+# bg = cv.cvtColor(bg, cv.COLOR_BGR2GRAY).astype(np.float32)
+images = [cv.imread(f, cv.IMREAD_GRAYSCALE) for f in glob.glob("../exerciseMaterials/UCSD_Anomaly_Dataset.v1p2/UCSDped1/Test/Test016/*.tif")]
 for images in glob.iglob('../Data/Test016/*.tif'):
     img = cv.imread(images)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY).astype(np.float32)
@@ -14,6 +15,7 @@ for images in glob.iglob('../Data/Test016/*.tif'):
     cv.waitKey(0)
 
 # diff = np.abs(bg - img)
+images = [cv.imread(f, cv.IMREAD_GRAYSCALE) for f in glob.glob("../exerciseMaterials/UCSD_Anomaly_Dataset.v1p2/UCSDped1/Test/Test016/*.tif")]
 
 # cv.imshow('1', bg)
 # cv.waitKey(0)
