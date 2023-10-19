b5d7afffe03f4c4f8910f0f61978b53e0c3ee394
vismiroglou <vismiroglou@gmail.com>
2023-10-19 13:46:24 +0200

    latest version

--- Miniproject/src/config.py
+++ Miniproject/src/config.py
@@ -2,19 +2,14 @@ from argparse import Namespace
 hparams = Namespace(**{'experiment_name': 'object_detection',
                        
                        # Task
-                       'train_path': 'data/',
-                       'val_path': '../../data/extracted/test',
-                       'test_path': '../data/2023/test',
+                       'train_path': 'data/train',
                        'output_dir': 'products/',
                        # Network
                        'num_classes': 5,
-                       'pretrained': False,
-                       'loggers': 'tensorboard', # 'tensorboard, wandb, flowml'
-                       'log_embeddings': False,
                        # Training
-                       'max_epochs': 2,
-                       'learning_rate': 1e-3,
-                       'batch_size': 48,
+                       'max_epochs': 5,
+                       'learning_rate': 1e-2,
+                       'batch_size': 5,
                        #'loss': 'mse',
                        'temperature': 0.1,
                        'n_workers':8,
