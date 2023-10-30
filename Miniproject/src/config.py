285843efcafce6ef4c4d498a22737be5b28d23ff
vismiroglou <vismiroglou@gmail.com>
2023-10-30 13:39:34 +0100

    minor changes

--- Miniproject/src/config.py
+++ Miniproject/src/config.py
@@ -1,22 +1,24 @@
 from argparse import Namespace
 hparams = Namespace(**{'experiment_name': 'object_detection',
                        
-                       # Task
-                       'train_path': 'data/train',
-                       'output_dir': 'products/',
-                       # Network
-                       'num_classes': 5,
-                       # Training
-                       'max_epochs': 5,
-                       'learning_rate': 1e-2,
-                       'batch_size': 5,
-                       #'loss': 'mse',
-                       'temperature': 0.1,
-                       'n_workers':8,
-                       'accelerator':'gpu',
-                       'device':'cuda', 
-                       # Logs
+                       # Directories
+                       'data': 'data/full', #Use when random_split
                        
+                       'train_path': 'data/train', #Use for separate datasets
+                       'val_path': 'data/test', #Use for separate datasets
+                    #    'test_path': 
+
+                       'output_dir': 'products/',
+
+                       # Network
+                       'num_classes': 5, #includes background
+
+                       # Training
+                       'max_epochs': 20,
+                       'learning_rate': 1e-4,
+                       'batch_size': 5,
+                       'n_workers':8,
+                       'device':'cuda',
                        
                        }
                        )
